776b7544bebbc5b51c67833c1ecb9eae70d8955f
Andreas Gompos <andreas.gompos@gmail.com>
2018-07-28 18:57:19 +0100

    removed unused bucket

--- infrastructure/core_infrastructure/buckets.tf
+++ infrastructure/core_infrastructure/buckets.tf
@@ -1,11 +1,3 @@
-resource "aws_s3_bucket" "serving_ai_bucket" {
-  bucket = "datagusto-serving-ai"
-  acl    = "private"
-  tags {
-    Name        = "serving ai bucket"
-  }
-}
-
 resource "aws_s3_bucket" "datagusto_playground_bucket" {
   bucket = "datagusto-playground"
   acl    = "private"
