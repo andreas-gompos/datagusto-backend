9e394e5e7f8a70ef1ace58f532193ebaf9bbac7a
Andreas Gompos <andreas.gompos@gmail.com>
2018-07-28 14:54:50 +0100

    now hosting on s3

--- infrastructure/core_infrastructure/iam_group_memberships.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_group_membership" "serving_ai_group" {
-  name = "serving_ai-membership"
-
-  users = [
-    "${aws_iam_user.ioannis_gkioulekas.name}"
-  ]
-
-  group = "${aws_iam_group.serving_ai_group.name}"
-}
--- infrastructure/core_infrastructure/iam_users.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user" "ioannis_gkioulekas" {
-  name = "ioannis.gkioulekas"
-  path = "/user/"
-}
